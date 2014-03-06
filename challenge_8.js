401e6ec0184509983213d44a7c36b8a60c17cc6c
jahsan87 <jahsan87@gmail.com>
2014-03-06 10:26:09 -1000

    commented code and tests

--- challenge_8.js
+++ challenge_8.js
@@ -3,35 +3,45 @@
 exports.LargestProduct = LargestProduct;
 
 function LargestProduct(n) {
+    //put number into an array
     this.n = n.split('');
-    this.largest = 0;
-    console.log(this.n[364] * this.n[365] * this.n[366] * this.n[367] * this.n[368]);
+    
+    //returns the largest product of 5 consecutive digits
     this.getLargestProduct = function() {
+        //initialize storage variables
         var largest = 0;
         var position = 0;
         var product = 0;
 
+        //loops through all digits to multiply 5 consecutive digits
         for (var i = 0; i < 1000; i++) {
             product = this.n[i] * this.n[i+1] * this.n[i+2] * this.n[i+3] * this.n[i+4];
+            
+            //if product greater than largest value, store product
             if (product > largest) {
                 largest = product;
-                //console.log("largest: "+largest);
+                console.log("largest: "+largest);
             }
         }
         return largest;
     }
 
+    //returns the position of the first consecutive digit of the greatest product
     this.getLargestProductPosition = function() {
+        //initialize storage variables
         var largest = 0;
         var position = 0;
         var product = 0;
 
+        //loops through all digits to multiply 5 consecutive digits
         for (var i = 0; i < 1000; i++) {
             product = this.n[i] * this.n[i+1] * this.n[i+2] * this.n[i+3] * this.n[i+4];
+            
+            //if product greater than largest value, store product and store current index
             if (product > largest) {
                 largest = product;
                 position = i;
-                //console.log("position: "+position);
+                console.log("position: "+position);
             }
         }
         return position;
